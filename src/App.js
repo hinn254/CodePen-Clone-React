454cb06cd2cb0a873ad47b20c4363e34307d9aee
Benny <bennyhinnotieno@gmail.com>
2020-09-22 01:09:14 +0300

    finalised components

--- src/App.js
+++ src/App.js
@@ -1,25 +1,64 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import "./App.css";
+import Editor from "./Editor";
+import useLocalStorage from "./hooks/useLocalStorage";
 
 function App() {
+  const [html, setHtml] = useLocalStorage("html", "");
+  const [css, setCss] = useLocalStorage("css", "");
+  const [javascript, setJavascript] = useLocalStorage("js", "");
+  const [srcDoc, setSrcDoc] = useState("");
+
+  useEffect(() => {
+    const timeout = setTimeout(() => {
+      setSrcDoc(` <html>
+      <body>${html}</body>
+      <style>${css}</style>
+      <script>${javascript}</script>
+    </html>`);
+    }, 300);
+    return () => clearTimeout(timeout);
+  }, [html, css, javascript]);
+
+  //   const srcDoc = ` <html>
+  //    <body>${html}</body>
+  //    <style>${css}</style>
+  //    <script>${javascript}</script>
+  //  </html>`;
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div className="pane top-pane">
+        <Editor
+          language="xml"
+          displayName="html"
+          value={html}
+          onChange={setHtml}
+        />
+        <Editor
+          language="css"
+          displayName="CSS"
+          value={css}
+          onChange={setCss}
+        />
+        <Editor
+          language="javascript"
+          displayName="JS"
+          value={javascript}
+          onChange={setJavascript}
+        />
+      </div>
+      <div className="pane">
+        <iframe
+          srcDoc={srcDoc}
+          title="output"
+          sandbox="allow-scripts"
+          frameBorder="0"
+          width="100%"
+          height="100%"
+        />
+      </div>
+    </>
   );
 }
 
